63289cab092cb88f490d3d9831b04109060b4175
vatsalya singh <vatsalyasingh218@gmail.com>
2021-02-17 11:55:28 +0530

    refactoring og fileEqual property

--- plato-core/src/main/java/org/techpleiad/plato/core/domain/BranchProfileReport.java
+++ plato-core/src/main/java/org/techpleiad/plato/core/domain/BranchProfileReport.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Getter
 public class BranchProfileReport {
     private String profile;
-    private Boolean fileEqual;
+    private boolean fileEqual;
     private Boolean propertyValueEqual;
     private List<Document> documents;
 }
