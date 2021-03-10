c919396cc335e67e2accdf3e63fb3028f04a7414
Rajat Goel <rajatgl17@gmail.com>
2021-03-10 16:49:03 +0530

    Added log related dependencies

--- plato-core/src/main/java/org/techpleiad/plato/core/domain/GitRepository.java
+++ plato-core/src/main/java/org/techpleiad/plato/core/domain/GitRepository.java
@@ -1,12 +1,16 @@
 package org.techpleiad.plato.core.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GitRepository {
 
     private String url;
