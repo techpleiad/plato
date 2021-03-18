acb15f1d6ee49f1d2814b7225c3a2af4f9c78cc6
vatsalya singh <vatsalyasingh218@gmail.com>
2021-03-18 12:22:29 +0530

    Created Mapper for consistency Level Report

--- plato-adapter/src/main/java/org/techpleiad/plato/adapter/mapper/ServiceManagerMapper.java
+++ plato-adapter/src/main/java/org/techpleiad/plato/adapter/mapper/ServiceManagerMapper.java
@@ -4,22 +4,24 @@ package org.techpleiad.plato.adapter.mapper;
 import org.mapstruct.Mapper;
 import org.techpleiad.plato.api.request.ServiceRequestTO;
 import org.techpleiad.plato.api.response.BranchProfileReportResponseTO;
-import org.techpleiad.plato.api.response.BranchResponseTO;
+import org.techpleiad.plato.api.response.BranchReportResponseTO;
+import org.techpleiad.plato.api.response.ConsistencyLevelValidateResponseTO;
 import org.techpleiad.plato.api.response.DocumentResponseTO;
 import org.techpleiad.plato.api.response.ProfilePropertiesResponseTO;
 import org.techpleiad.plato.api.response.ServiceResponseTO;
 import org.techpleiad.plato.api.response.ServicesAcrossBranchValidateResponseTO;
 import org.techpleiad.plato.api.response.ServicesAcrossProfileValidateResponseTO;
 import org.techpleiad.plato.core.domain.BranchProfileReport;
+import org.techpleiad.plato.core.domain.BranchReport;
 import org.techpleiad.plato.core.domain.ConsistencyAcrossBranchesReport;
 import org.techpleiad.plato.core.domain.ConsistencyAcrossProfilesReport;
+import org.techpleiad.plato.core.domain.ConsistencyLevelAcrossBranchesReport;
 import org.techpleiad.plato.core.domain.Document;
 import org.techpleiad.plato.core.domain.ServiceSpec;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface ServiceManagerMapper {
@@ -28,13 +30,20 @@ public interface ServiceManagerMapper {
 
     ServiceResponseTO convertServiceSpecToServiceResponseTO(final ServiceSpec serviceSpec);
 
+    List<ConsistencyLevelValidateResponseTO> ConsistencyLevelBranchesReportToConsistencyLevelResponse(final List<ConsistencyLevelAcrossBranchesReport> consistencyLevelAcrossBranchesReport);
+
+    List<BranchReportResponseTO> convertBranchReportToBranchReportResponse(final List<BranchReport> branchReports);
+
     List<ServiceResponseTO> convertServiceSpecListToServiceResponseTOList(List<ServiceSpec> serviceSpecs);
 
     List<DocumentResponseTO> convertDocumentToDocumentResponseTo(final List<Document> document);
 
     List<BranchProfileReportResponseTO> convertBranchProfileToBranchProfileResponse(final List<BranchProfileReport> branchProfileReport);
 
-    List<ServicesAcrossBranchValidateResponseTO> convertConsistencyAcrossBranchesReportToServicesAcrossBranchValidateResponseTO(final List<ConsistencyAcrossBranchesReport> consistencyAcrossBranchesReport);
+    List<ServicesAcrossBranchValidateResponseTO> convertConsistencyAcrossBranchesReportListToServicesAcrossBranchListValidateResponseTO(final List<ConsistencyAcrossBranchesReport> consistencyAcrossBranchesReport);
+
+    ServicesAcrossBranchValidateResponseTO convertConsistencyAcrossBranchesReportToServicesAcrossBranchValidateResponseTO(final ConsistencyAcrossBranchesReport consistencyAcrossBranchesReport);
+
 
     default ServicesAcrossProfileValidateResponseTO convertInconsistentProfilePropertyToProfilePropertyResponseTO(
             final String service, final String branch, final ConsistencyAcrossProfilesReport profilePropertyDetails) {
@@ -52,10 +61,10 @@ public interface ServiceManagerMapper {
                         profilePropertiesResponseTOList.add(ProfilePropertiesResponseTO.builder()
                                         .properties(properties)
                                         .document(DocumentResponseTO.builder().
-                                                profile(profile)
+                                                        profile(profile)
 //                                                .document(profileDocument.get(profile))
                                                         .build()
-                                                )
+                                        )
                                         .build()
                         )
         );
