e65569b5d7632cb674b880f0dd95b226aed3b6d5
vatsalya singh <vatsalyasingh218@gmail.com>
2021-03-23 14:47:37 +0530

    added feature to show profile if the yaml file is not present

--- plato-core/src/main/java/org/techpleiad/plato/core/service/NoEmailService.java
+++ plato-core/src/main/java/org/techpleiad/plato/core/service/NoEmailService.java
@@ -1,15 +1,10 @@
 package org.techpleiad.plato.core.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.techpleiad.plato.core.port.in.IEmailServiceUseCase;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import java.util.List;
 
 @Service
@@ -17,21 +12,8 @@ import java.util.List;
 @ConditionalOnProperty(value = "plato.email.enabled", havingValue = "false", matchIfMissing = true)
 public class NoEmailService implements IEmailServiceUseCase {
 
-    @Autowired
-    private JavaMailSender javaMailSender;
-
     @Override
     public void sendEmail(final String mailBody, final List<String> recipient, final String subject, final String from) {
-        try {
-            final MimeMessage message = javaMailSender.createMimeMessage();
-            final MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom(from);
-            helper.setSubject(subject);
-            helper.setTo(recipient.toArray(new String[recipient.size()]));
-            helper.setText(mailBody, true);
-            javaMailSender.send(message);
-        } catch (final MessagingException e) {
-            log.error("Unable to send email : {}", e);
-        }
+        log.info("Send email : {} to : {}", mailBody, recipient);
     }
 }
