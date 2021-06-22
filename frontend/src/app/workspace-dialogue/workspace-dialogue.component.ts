433820e22887a90f5a5536f5e7052bade0094eef
Abhishek Garg <abhishekgarg.14august@gmail.com>
2021-06-22 11:43:44 +0530

    Fix validation bug

--- frontend/src/app/workspace-dialogue/workspace-dialogue.component.ts
+++ frontend/src/app/workspace-dialogue/workspace-dialogue.component.ts
@@ -118,6 +118,7 @@ export class WorkspaceDialogueComponent implements OnInit {
     
 
     this.missingProperties = [];
+    this.reqValidation = true;
     
     this.setBranchProfileReq();
   }
@@ -342,7 +343,8 @@ export class WorkspaceDialogueComponent implements OnInit {
     if(this.isBranchReq===true && this.branchValue===""){
       this.reqValidation = false;
     }
-    if(this.canProfileDefault===false && this.profileValue===""){
+    if(this.canProfileDefault===false && this.isProfileReq===true && this.profileValue===""){
+      console.log("hello");
       this.reqValidation = false;
     }
     if(this.isBranch1Req===true && this.sourceBranchValue===""){
